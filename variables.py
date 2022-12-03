b203699d08e1d548722d4003fdfa437d45bfb878
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-03 21:52:20 +0100

    input function

--- variables.py
+++ variables.py
@@ -6,6 +6,24 @@
 # print(type(is_girl))
 
 
+# first_name = "Ibrahim"
+# last_name = "Ibrosci"
+# age = str(1000)
+# is_boy = str(True)
+
+# print("Hello "+ first_name+ " "+ last_name + ", You are " + age + " years old. and a boy is "+ is_boy)
+
+
+
+first_name = input("Enter Firstname: ")
+last_name = input("Enter Lastname: ")
+age = input("Enter Age: ")
+is_boy = False
+
+if is_boy == True:
+    print("Hello "+ first_name+ " "+ last_name + ", You are " + age + " years old.")
+else:
+    print("You are not a boy")
 
 
 
