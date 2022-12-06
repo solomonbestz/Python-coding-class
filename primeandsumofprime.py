4a64c20b0afe1f7d34c937eaf288046003ad267c
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-06 12:01:35 +0100

    prime and sum of prime python

---
+++ primeandsumofprime.py
@@ -0,0 +1,31 @@
+# number = 7
+
+# say = True
+
+# if  number > 1:
+#     for i in range(2, number // 2 + 1):
+#         if number % i == 0:
+#             say = False
+#             break
+#     if say:
+#         print(f"{number} is prime")
+#     else:
+#         print(f'{number} not prime')
+# else:
+#     print(f'{number} not prime')
+
+# number = 60
+# sum_number = 0
+
+# for num in range(2, number + 1):
+#     index = 1
+#     for n in range(2, num // 2 + 1):
+#         if num % n == 0:
+#             print(f"{num} is not a prime")
+#             index = num
+#             break;
+#     if index is not num:
+#         print(num)
+#         sum_number += num
+# print(f"{sum_number} is the sum of {number}")
+
