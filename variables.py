e137802bd9e250388e36dd1bd626a4b759fec04c
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-12-22 18:05:13 +0100

    modified our bag store

--- variables.py
+++ variables.py
@@ -24,3 +24,42 @@ When a value is first assigned to a variable, we have then declared a variable
 """
 
 first_bag_product = "Gucci"
+
+# VARIABLE DATATYPES
+
+"""
+There are several datatypes, but i would be listing just the basic 4
+The String(str), The Integer(int), The floating(float), The boolean(bool)
+"""
+
+is_coding_fun = True # This is a boolean data type
+
+float_data_type = 34.56 # This is a float data type
+
+positive_int_data_type = 24 # This is a positive integer data type
+negative_int_data_type = -24 # This is a negative integer data type
+
+
+
+string_data_type1 = 'I am single quote' # This is a single quote string
+string_data_type2 = "I am double quote" # This is a double quote string
+string_data_type3 = '''I am triple quote''' # This is a triple quote string
+
+
+# LET US UPDATE OUR BAG STORE PROGRAM
+
+first_bag_name = "Gucci" # A string variable to hold the name of our bag product
+
+is_in_stock_first_bag = True # Checking if our bag is in stock
+
+price_tag_first_bag = 23000.00 # A float variable to hold our price tag
+
+quantity_first_bag = 20 # We have 20 Gucci bags in store
+
+
+
+
+
+
+
+
