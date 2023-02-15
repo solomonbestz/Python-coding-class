8cd4a74e3ac52ac4d62f59aba9ae5f0c7d9bb52a
Solomon Bestz <Kingsolodawarrior@gmail.com>
2023-02-15 17:21:50 +0100

    modified

--- conversion.py
+++ conversion.py
@@ -27,7 +27,35 @@ my_characters = {
     "x": "01111000",
     "y": "01111001",
     "z": "01111010",
-    " ": "01000000"
+    "A": "01000001",
+    "B": "01000010",
+    "C": "01000011",
+    "D": "01000100",
+    "E": "01000101",
+    "F": "01000110",
+    "G": "01000111",
+    "H": "01001000",
+    "I": "01001001",
+    "J": "01001010",
+    "K": "01001011",
+    "L": "01001100",
+    "M": "01001101",
+    "N": "01001110",
+    "O": "01001111",
+    "P": "01010000",
+    "Q": "01010001",
+    "R": "01010010",
+    "S": "01010011",
+    "T": "01010100",
+    "U": "01010101",
+    "V": "01010110",
+    "W": "01010111",
+    "X": "01011000",
+    "Y": "01011001",
+    "Z": "01011010",
+    " ": "01000000",
+    ",": "00101100",
+    ".": "00101110"
 }
 
 def convert(word):
@@ -44,7 +72,7 @@ def convert(word):
                 converted_charater.append(value)
     for conv in converted_charater:
         joined_character += conv + " "
-    return [words, joined_character]
+    return [words, joined_character] 
 
 def display(words, joined_character):
     print("Please wait while we convert your text ")
@@ -60,7 +88,7 @@ if __name__=="__main__":
     time.sleep(2)
     print('Write a message and we would convert it to binary number')
     time.sleep(2)
-    user_input = input('Write Text: ').lower()
+    user_input = input('Write Text: ')
     show = convert(user_input)
     display(show[0], show[1])
 
